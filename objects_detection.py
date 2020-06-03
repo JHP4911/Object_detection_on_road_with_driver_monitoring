77a108406066f7d04a5ff13c8e6ffa2d4c7d92af
Saini_Pri <61472375+priyankasaini24@users.noreply.github.com>
2020-06-03 21:35:10 +0530

    Update objects_detection.py

--- objects_detection.py
+++ objects_detection.py
@@ -1,27 +1,27 @@
-import jetson.inference
-import jetson.utils
-import sys
+import jetson.inference                                   # NVIDIA module for object detection
+import jetson.utils                                       # NVIDIA module for camera capture
+import sys						  # to call functions
 
-net= jetson.inference.detectNet("SSD-Mobilenet-v2", threshold=0.5)
+net= jetson.inference.detectNet("SSD-Mobilenet-v2", threshold=0.5)         # load the object detection model
 
-camera = jetson.utils.gstCamera(1280, 720, "/dev/video0")  # using V4L2
+camera = jetson.utils.gstCamera(1280, 720, "/dev/video0")                  # for camera capture
 
-display = jetson.utils.glDisplay()
+display = jetson.utils.glDisplay()                                         # create display with glDisplay object
 
 count=0
 while display.IsOpen():
 	
-	img, width, height = camera.CaptureRGBA()
+	img, width, height = camera.CaptureRGBA()                          # camera capture returning with width 
 	
-	detections = net.Detect(img, width, height)
+	detections = net.Detect(img, width, height)                        # perform detections, this takes img, width and height from camera.capture() 
 	if detections:
-		print("detected {:d} objects :".format(len(detections)))
+		print("detected {:d} objects :".format(len(detections)))   # print the detections 
 		count= count+1
 		for detection in detections:
-			print(detection)
-	display.RenderOnce(img, width, height)
+			print(detection)                                   # print Object Detection Result like classID
+	display.RenderOnce(img, width, height)                             # render the image
 
-	display.SetTitle("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))	  	
+	display.SetTitle("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))  # window title anf Fps	  	
 
 print("Total number of objects detected:"),	
 print(count)
